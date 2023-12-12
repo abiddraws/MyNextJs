053feca718dbb581baf79401edf82487319dc4c2
abiddraws <142387683+abiddraws@users.noreply.github.com>
2023-12-12 13:59:49 +0400

    Update to next 14

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,71 @@
-import createMiddleware from 'next-intl/middleware';
-import {locales} from './navigation';
- 
-export default createMiddleware({
-  defaultLocale: 'en',
-  locales
-});
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import createIntlMiddleware from "next-intl/middleware";
+import { createServerClient, type CookieOptions } from "@supabase/ssr";
+import { locales } from "./navigation";
+
+export async function middleware(req: NextRequest) {
+  const handleI18nRouting = createIntlMiddleware({
+    locales: locales,
+    defaultLocale: "en",
+  });
+  const res = handleI18nRouting(req);
+  let response = NextResponse.next({
+    request: {
+      headers: req.headers,
+    },
+  });
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return req.cookies.get(name)?.value;
+        },
+        set(name: string, value: string, options: CookieOptions) {
+          req.cookies.set({
+            name,
+            value,
+            ...options,
+          });
+          response = NextResponse.next({
+            request: {
+              headers: req.headers,
+            },
+          });
+          response.cookies.set({
+            name,
+            value,
+            ...options,
+          });
+        },
+        remove(name: string, options: CookieOptions) {
+          req.cookies.set({
+            name,
+            value: "",
+            ...options,
+          });
+          response = NextResponse.next({
+            request: {
+              headers: req.headers,
+            },
+          });
+          response.cookies.set({
+            name,
+            value: "",
+            ...options,
+          });
+        },
+      },
+    },
+  );
+
+  await supabase.auth.getSession();
+
+  return res;
+}
+
+export const config = {
+  matcher: ["/((?!api|_next|.*\\..*).*)"],
+};
